c1d0d0cfefa099a63833d1babd503789184df8d9
Laxmi Chari <143387331+laxmi1763@users.noreply.github.com>
2024-10-14 00:11:47 +0530

    Update Session.java with necessary  comments

--- goaLegislativeAssembly/src/goaLegislativeAssembly/Session.java
+++ goaLegislativeAssembly/src/goaLegislativeAssembly/Session.java
@@ -3,24 +3,24 @@ package goaLegislativeAssembly;
 //AUTHOR NAME: LAXMI CHARI
 //ROLL NO: 22
 //TITLE: IMITATION OF GOA LEGISLATIVE ASSEMBLY
-//START DATE: 15/072024
+//START DATE: 15/07/2024
 //MODIFIED DATE: 22/07/2024
 //DESCRIPTION: THIS CLASS EXTENDS ENTITY CLASS AND HOLDS THE DETAILS ABOUT THE SESSIONS HELD IN THE ASSEMBLY
 
+//Session class representing a legislative session
 public class Session extends Entity {
-  private String date;
-  private String agenda;
+	//Inheritance: Session a class that is derived from a abstract class Entity
+private String date;
+private String agenda;
 
-  public  Session(String date, String agenda){
-      this.date=date;
-      this.agenda=agenda;
-  }
+public Session(String date, String agenda) {
+	//Constructor
+   this.date = date;
+   this.agenda = agenda;
+}
 
-  @Override
-  public String toString() {
-    return "Date of the session: " + date + "\n Agenda:  " + agenda;
-  }
-
-
-
+@Override
+public String toString() {
+   return "Date of the session: " + date + "\nAgenda: " + agenda;
+}
 }
