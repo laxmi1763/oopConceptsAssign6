53cb04647c2530b25e4967f718395f2e7c574b6f
Laxmi Chari <143387331+laxmi1763@users.noreply.github.com>
2024-10-14 00:11:18 +0530

    Update Official.java with necessary comments

--- goaLegislativeAssembly/src/goaLegislativeAssembly/Official.java
+++ goaLegislativeAssembly/src/goaLegislativeAssembly/Official.java
@@ -3,21 +3,24 @@ package goaLegislativeAssembly;
 //AUTHOR NAME: LAXMI CHARI
 //ROLL NO: 22
 //TITLE: IMITATION OF GOA LEGISLATIVE ASSEMBLY
-//START DATE: 15/072024
+//START DATE: 15/07/2024
 //MODIFIED DATE: 22/07/2024
 //DESCRIPTION: THIS CLASS EXTENDS ENTITY CLASS AND IT IS MADE FOR THE DETAILS OF THE OFFICIALS
 
+//Official class representing a legislative official like Speaker or Governor
 public class Official extends Entity {
-  private String position;
-  private String name;
+	//Inheritance: Official a class that is derived from a abstract class Entity
+private String position;
+private String name;
 
-  public Official(String position, String name) {
-      this.position = position;
-      this.name = name;
-  }
+public Official(String position, String name) {
+	//Constructor
+   this.position = position;
+   this.name = name;
+}
 
-  @Override
-  public String toString() {
-      return "Position: " + position + "\nName: " + name;
-  }
+@Override
+public String toString() {
+   return "Position: " + position + "\nName: " + name;
+}
 }
