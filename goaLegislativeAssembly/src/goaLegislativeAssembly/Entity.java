83741c76d6be45753dc3a4094e6e94bf515d6bcd
Laxmi Chari <143387331+laxmi1763@users.noreply.github.com>
2024-10-14 00:10:04 +0530

    Update Entity.java with necessary comments

--- goaLegislativeAssembly/src/goaLegislativeAssembly/Entity.java
+++ goaLegislativeAssembly/src/goaLegislativeAssembly/Entity.java
@@ -3,11 +3,14 @@ package goaLegislativeAssembly;
 //AUTHOR NAME: LAXMI CHARI
 //ROLL NO: 22
 //TITLE: IMITATION OF GOA LEGISLATIVE ASSEMBLY
-//START DATE: 15/072024
+//START DATE: 15/07/2024
 //MODIFIED DATE: 22/07/2024
 //DESCRIPTION: THIS IS AN ABSTRACT CLASS TO BE EXTENDED BY FOLLOWING CLASSES IN THIS PROJECT 
 
+//Abstract base class for entities like Bill, MLA, Official, Session
 public abstract class Entity {
-  @Override
-  public abstract String toString();
+	//Abstraction: The Entity class is abstract, meaning it cannot be instantiated directly. It provides a template for other classes (like Bill, MLA, Official, and Session) to follow.
+	//It defines the structure and behavior that all subclasses must implement, but it doesn't provide any implementation details itself.
+@Override
+public abstract String toString();
 }
