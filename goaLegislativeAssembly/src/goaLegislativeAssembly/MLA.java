5bd7ed09a331b8f402c74ae901ea34cfb6a33800
Laxmi Chari <143387331+laxmi1763@users.noreply.github.com>
2024-10-14 00:10:56 +0530

    Update MLA.java with necessary comments

--- goaLegislativeAssembly/src/goaLegislativeAssembly/MLA.java
+++ goaLegislativeAssembly/src/goaLegislativeAssembly/MLA.java
@@ -7,19 +7,22 @@ package goaLegislativeAssembly;
 //MODIFIED DATE: 22/07/2024
 //DESCRIPTION: THIS CLASS EXTENDS ENTITY CLASS AND HOLDS THE DETAILS ABOUT MLA
 
+//MLA class representing a Member of Legislative Assembly
 public class MLA extends Entity {
-  private String name;
-  private String constituency;
-  private String party;
+	//Inheritance: MLA a class that is derived from a abstract class Entity
+private String name;
+private String constituency;
+private String party;
 
-  public MLA(String name, String constituency, String party) {
-      this.name = name;
-      this.constituency = constituency;
-      this.party=party;
-  }
+public MLA(String name, String constituency, String party) {
+	//Constructor
+   this.name = name;
+   this.constituency = constituency;
+   this.party = party;
+}
 
-  @Override
-  public String toString() {
-      return "Name: " + name + "\nConstituency: " + constituency + "\nPolitical party: " + party;
-  }
+@Override
+public String toString() {
+   return "Name: " + name + "\nConstituency: " + constituency + "\nPolitical party: " + party;
+}
 }
