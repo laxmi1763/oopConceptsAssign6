9e028f0f8e9e6328309c0ca272f9ba179eb774e0
Laxmi Chari <143387331+laxmi1763@users.noreply.github.com>
2024-10-14 00:09:35 +0530

    Update Bill.java with necessary comments

--- goaLegislativeAssembly/src/goaLegislativeAssembly/Bill.java
+++ goaLegislativeAssembly/src/goaLegislativeAssembly/Bill.java
@@ -3,27 +3,31 @@ package goaLegislativeAssembly;
 //AUTHOR NAME: LAXMI CHARI
 //ROLL NO: 22
 //TITLE: IMITATION OF GOA LEGISLATIVE ASSEMBLY
-//START DATE: 15/072024
+//START DATE: 15/07/2024
 //MODIFIED DATE: 22/07/2024
 //DESCRIPTION: THIS CLASS EXTENDS ENTITY CLASS AND HOLDS THE DETAILS ABOUT THE BILLS
 
+//Bill class representing a legislative bill
 public class Bill extends Entity {
-  private String title;
-  private String description;
-  private boolean isPassed;
+	//Inheritance: Bill a class that is derived from an abstract class Entity
+private String title;
+private String description;
+private boolean isPassed;
 
-  public Bill(String title, String description) {
-      this.title = title;
-      this.description = description;
-      this.isPassed = false;
-  }
+public Bill(String title, String description) {
+	//Constructor
+   this.title = title;
+   this.description = description;
+   this.isPassed = false; // Initially the bill is not passed
+}
 
-  public void pass() {
-      this.isPassed = true;
-  }
+// Method to pass the bill
+public void pass() {
+   this.isPassed = true;
+}
 
-  @Override
-  public String toString() {
-      return "Title: " + title + "\nDescription: " + description + "\nStatus: " + (isPassed ? "Passed" : "Not Passed");
-  }
+@Override
+public String toString() {
+   return "Title: " + title + "\nDescription: " + description + "\nStatus: " + (isPassed ? "Passed" : "Not Passed");
+}
 }
